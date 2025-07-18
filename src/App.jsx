e092c6f7e04a1e9228099b4c7011f7207f75aabd
Shaimaa01 <shisko939@gmail.com>
2025-07-18 22:48:41 +0300

    fix(build): resolve incorrect import path for Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { ExtensionsList } from "./components/Extensions/ExtensionsList";
-import { Header } from "./components/header/header";
+import { Header } from "./components/header/Header";
 import { ThemeProvider } from "./context/ThemeContext";
 
 function App() {
